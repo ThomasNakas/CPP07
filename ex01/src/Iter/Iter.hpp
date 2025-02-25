58a9f6cc09de06d27166c3c3555ccc7b1517b578
Thomas Nakas <tnakas@3-E-3.42heilbronn.de>
2025-02-25 15:31:40 +0100

    changing the makefile according to the ex00 logic

---
+++ ex01/src/Iter/Iter.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/25 15:12:17 by tnakas            #+#    #+#             */
+/*   Updated: 2025/02/25 15:21:40 by tnakas           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+#define ITER_HPP
+
+#include <iostream>
+
+template <typename T>
+void iter(T* array, size_t length, void(*func)(T&))
+{
+	for (size_t i = 0; i < legth; ++i)
+		func(array[i]);
+}
+
+template <typename T>
+void printElement(T& element)
+{
+	std::cout << element << " ";
+}
+
+template <typename T>
+
+void DoubleElement(T& element)
+{
+	element *= 2;
+}
+#endif
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,14 +6,38 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 22:15:33 by tnakas            #+#    #+#             */
-/*   Updated: 2025/02/25 12:05:44 by tnakas           ###   ########.fr       */
+/*   Updated: 2025/02/25 15:30:25 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "Serial/Serializer.hpp"
+#include "Iter/Iter.hpp"
 
 int main()
 {
+	//Array of integers
+	int intArray[] = {1, 2, 3, 4, 5};
+	size_t intArrayLength = sizeof(intArray) / sizeof(intArray[0]);
+	
+	std::cout << "Original integer array: \n";
+	iter(intArray, intArrayLength, printElement);
+	std::cout << "\n";
+
+	std::cout << "Doubled integer array: ";
+	iter(intArray, intArrayLength, DoubleElement);
+	iter(intArray, intArrayLength, printElement);
+	std::cout << "\n";
+	//Array of strings
+
+	std::string strArray[] = {"Hello", "World", "Iter", "Template"};
+	size_t strArrayLength = sizeof(strArray) / sizeof(strArray[0]);
+	
+ 	std::cout << "Original strings array: \n";
+	iter(strArray, strArrayLength, printElement);
+	std::cout << "\n";
+	std::cout << "Doubled strings array: ";
+	iter(strArray, strArrayLength, DoubleElement);
+	iter(strArray, strArrayLength, printElement);
+	std::cout << "\n";
 	return 0;
 	
 }
