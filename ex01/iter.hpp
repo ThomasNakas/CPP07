5a582b1f05dd2183b970ad27feece1dc6b53e17e
Thomas Nakas <tnakas@3-E-8.42heilbronn.de>
2025-02-26 00:22:20 +0100

    overloading the iter to work for constants and non constant T types inputs

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Iter.hpp                                           :+:      :+:    :+:   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 15:12:17 by tnakas            #+#    #+#             */
-/*   Updated: 2025/02/25 15:32:59 by tnakas           ###   ########.fr       */
+/*   Updated: 2025/02/26 00:20:04 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,38 @@
 #define ITER_HPP
 
 #include <iostream>
+#include <cstddef>
+// template <typename T>
+// void iter(T* array, size_t length, void(*func)(const T&))
+// {
+// 	for (size_t i = 0; i < length; ++i)
+// 		func(array[i]);
+// };
 
 template <typename T>
-void iter(T* array, size_t length, void(*func)(T&))
+void iter(T* array, size_t length, void (*func)(T& thing))
 {
 	for (size_t i = 0; i < length; ++i)
 		func(array[i]);
-}
+};
+
+template <typename T>
+void iter(T* array, size_t length, void (*func)(const T& thing))
+{
+	for (size_t i = 0; i < length; ++i)
+		func(array[i]);
+};
 
 template <typename T>
 void printElement(T& element)
 {
 	std::cout << element << " ";
-}
+};
 
 template <typename T>
 
 void DoubleElement(T& element)
 {
 	element *= 2;
-}
+};
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,33 +6,67 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 22:15:33 by tnakas            #+#    #+#             */
-/*   Updated: 2025/02/25 19:23:50 by tnakas           ###   ########.fr       */
+/*   Updated: 2025/02/26 00:20:42 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
-int main()
-{
-	//Array of integers
-	int intArray[] = {1, 2, 3, 4, 5};
-	size_t intArrayLength = sizeof(intArray) / sizeof(intArray[0]);
+// int main()
+// {
+// 	//Array of integers
+// 	int intArray[] = {1, 2, 3, 4, 5};
+// 	size_t intArrayLength = sizeof(intArray) / sizeof(intArray[0]);
 	
-	std::cout << "Original integer array: \n";
-	iter(intArray, intArrayLength, printElement);
-	std::cout << "\n";
+// 	std::cout << "Original integer array: \n";
+// 	iter(intArray, intArrayLength, printElement);
+// 	std::cout << "\n";
 
-	std::cout << "Doubled integer array: ";
-	iter(intArray, intArrayLength, DoubleElement);
-	iter(intArray, intArrayLength, printElement);
-	std::cout << "\n";
-	//Array of strings
-	std::string strArray[] = {"Hello", "World", "Iter", "Template"};
-	size_t strArrayLength = sizeof(strArray) / sizeof(strArray[0]);
+// 	std::cout << "Doubled integer array: ";
+// 	iter(intArray, intArrayLength, DoubleElement);
+// 	iter(intArray, intArrayLength, printElement);
+// 	std::cout << "\n";
+// 	//Array of strings
+// 	std::string strArray[] = {"Hello", "World", "Iter", "Template"};
+// 	size_t strArrayLength = sizeof(strArray) / sizeof(strArray[0]);
 	
- 	std::cout << "Running printElement in the strings array: \n";
-	iter(strArray, strArrayLength, printElement);
-	std::cout << "\n";
-	return 0;
-	
+//  	std::cout << "Running printElement in the strings array: \n";
+// 	iter(strArray, strArrayLength, printElement);
+// 	std::cout << "\n";
+// 	return 0;
+// }
+
+//ex01.cpp
+//-------------begin---------------------------
+class Awesome
+{
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+  o << rhs.get();
+  return o;
 }
+
+template< typename T >
+void print( T& x )
+{
+  std::cout << x << std::endl;
+  return;
+}
+
+int main() {
+  int tab[] = { 0, 1, 2, 3, 4};
+  Awesome tab2[5];
+
+  iter(tab, 5, print<const int>);
+  iter(tab2, 5, print<Awesome>);
+
+  return 0;
+}
+//-------------end---------------------------
