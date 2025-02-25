13cf00d4b57f702287ed42814bfc1452edf1c3c9
Thomas Nakas <tnakas@3-E-3.42heilbronn.de>
2025-02-25 15:06:56 +0100

    making more readable the tests ex00 debuged and completed

--- ex00/src/Temp/Temp.hpp
+++ ex00/src/Temp/Temp.hpp
@@ -6,7 +6,7 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 14:33:54 by tnakas            #+#    #+#             */
-/*   Updated: 2025/02/25 14:37:08 by tnakas           ###   ########.fr       */
+/*   Updated: 2025/02/25 14:53:38 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,22 @@
 
 #include <iostream>
 template <typename T>
-void swap(T& a, T& b);
+void swap(T& a, T& b)
+{
+	T temp = a;
+	a = b;
+	b = temp;
+};
 
 template <typename T>
-const T& min(const T& a, const T& b);
+const T& min(const T& a, const T& b)
+{
+	return (a < b) ? a : b;
+};
 
 template <typename T>
-const T& max(const T& a, const T& b);
+const T& max(const T& a, const T& b)
+{
+	return (a > b) ? a : b;
+};
 #endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tnakas <tnakas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 22:15:33 by tnakas            #+#    #+#             */
-/*   Updated: 2025/02/25 14:46:00 by tnakas           ###   ########.fr       */
+/*   Updated: 2025/02/25 15:06:12 by tnakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,26 @@
 
 int main()
 {
+	//int tests
 	int a = 2;
 	int b = 3;
-	
+	//More readable additions
+	std::cout << "---->Before Swap<-----\n";
+	std::cout << " a = " << a << ", b = " << b << "\n";
 	::swap(a, b);
+	std::cout << "---->After Swap<-----\n";
 	std::cout << " a = " << a << ", b = " << b << "\n";
 	std::cout << "min(a, b) = " << ::min(a, b) << "\n";
 	std::cout << "max(a, b) = " << ::max(a, b) << "\n";
 
+	//string tests
 	std::string c = "chaine1";
 	std::string d = "chaine2";
+	//More readable additions
+	std::cout << "---->Before Swap<-----\n";
+	std::cout << " c = " << c << ", d = " << d << "\n";
 	::swap(c, d);
+	std::cout << "---->After Swap<-----\n";
 	std::cout << " c = " << c << ", d = " << d << "\n";
 	std::cout << "min(c, d) = " << ::min(c, d) << "\n";
 	std::cout << "max(c, d) = " << ::max(c, d) << "\n";
